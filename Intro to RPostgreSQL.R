ac7793672a3315cab98c1a6c5a41072edc9c6225
datatroy <troy.adair@datadicts.com>
2019-06-18 11:17:24 -0400

    Commit of Intro to RPostgreSQL

---
+++ Intro to RPostgreSQL.R
@@ -0,0 +1,16 @@
+# Create a connection to the database
+library('RPostgreSQL')
+
+## Loading required package: DBI
+
+pg = dbDriver("PostgreSQL")
+
+# Local Postgres.app database; no password by default
+# Of course, you fill in your own database information here.
+con = dbConnect(pg, user="postgres", password="",
+                host="localhost", port=5432, dbname="Yellow_Taxi")
+dbListTables(con)
+dbListFields(con, "temp")
+# rs <- dbSendQuery(con,"SELECT * from temp")
+long_trips <- dbGetQuery(con, "select * from temp where trip_distance > 100")
+head(long_trips)
